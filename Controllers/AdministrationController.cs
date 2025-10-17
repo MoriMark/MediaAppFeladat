6c935a68e5f4f088259492334f108e08389c31c3
MoriMark <morimark20@gmail.com>
2025-10-17 13:23:18 +0200

    Administration, Create, Delete Users

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,150 @@
+﻿using MediaAppFeladat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MediaAppFeladat.ViewModels;
+using MediaAppFeladat.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace MediaAppFeladat.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AdministrationController(AppDbContext context, SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            _context = context;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.FullName,
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("Index", "Administration");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Email, FullName, PasswordHash")] Users user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    user.UserName = user.Email;
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Delete(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(string id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+
+    }
+}
---
+++ ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaAppFeladat.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
